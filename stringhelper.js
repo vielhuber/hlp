60242970fc3a32a2ac738ee523476938f0f5ad16
David Vielhuber <david@vielhuber.de>
2017-12-12 00:40:23 +0100

    polished readme

--- stringhelper.js
+++ stringhelper.js
@@ -1,6 +1,6 @@
 function __x(input)
 {
-    if( input === null || (typeof input === 'string' && input.trim() == '') || (typeof input === 'object' && Object.keys(input).length === 0 && input.constructor === Object) || (typeof input === 'undefined') || (Array.isArray(input) && input.length === 0) ) { return false; }
+    if( input === null || input === false || (typeof input === 'string' && input.trim() == '') || (typeof input === 'object' && Object.keys(input).length === 0 && input.constructor === Object) || (typeof input === 'undefined') || (Array.isArray(input) && input.length === 0) ) { return false; }
     if( Array.isArray(input) && input.length === 1 && input[0] === '' ) { return false; }
     return true;
 }
