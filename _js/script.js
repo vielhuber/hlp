b679f3c79c95f28e44c2e79f02fc7768e1b63062
David Vielhuber <david@vielhuber.de>
2018-08-25 23:56:06 +0200

    Improve filetobase64

--- _js/script.js
+++ _js/script.js
@@ -1409,7 +1409,7 @@ export default class hlp
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
             reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
+            reader.onload = () => resolve(reader.result.split(',')[1]);
             reader.onerror = error => reject(error);
         });
     }
